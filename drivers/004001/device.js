fe23ac4a5b25a20b91632a92b7124c00bff194eb
Marco Ruiter <mruiter@tuxweb.nl>
2017-08-29 17:28:50 +0200

    More

--- drivers/004001/device.js
+++ drivers/004001/device.js
@@ -8,8 +8,9 @@ class P004001 extends ZwaveDevice {
 		this.enableDebug();
 		this.printNode();
 		this.registerCapability('onoff', 'SWITCH_BINARY');
-		this.registerCapability('alarm_tamper', 'SENSOR_BINARY');	
-		this.registerCapability('alarm_tamper', 'BATTERY');
+		this.registerCapability('alarm_smoke', 'ALARM');	
+		this.registerCapability('alarm_tamper', 'ALARM');
+		this.registerCapability('alarm_battery', 'BATTERY');
 		this.registerCapability('measure_battery', 'BATTERY');
 	}
 
